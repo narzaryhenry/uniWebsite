667a4daba8e844182a88c147e71d04a2de9008dd
Henry Narzary <narzaryhenry0@gmail.com>
2024-11-01 10:54:28 +0530

    navbar and hero done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
